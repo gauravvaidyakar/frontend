27631fc69eca9c453a5234387c6b73ee09b7006a
gauravvaidyakar <gauravvaidyakar@gmail.com>
2026-08-07 17:49:15 +0530

    pankya

---
+++ pankya/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import myImage from'./assets/Gemini_Generated_Image_3m7be63m7be63m7b-removebg-preview.png'
+
+
+function App() {
+// const handlechange=()=>{
+//     let hanuman = document.querySelector("h1")
+//     hanuman.innerText = "hanuman is flying"
+// }
+
+  return (
+    
+  )
+}
+
+export default App
+ 
