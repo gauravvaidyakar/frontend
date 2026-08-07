7c8271038fbb59537c6f31de0851639155f62ee8
gauravvaidyakar <gauravvaidyakar@gmail.com>
2026-08-07 18:08:46 +0530

    rtpractice

---
+++ RT-practice/src/App.jsx
@@ -0,0 +1,11 @@
+import Counter from '../../Redux-toolkit-in-react/src/components/Counter'
+
+const App = () => {
+  return (
+    <>
+    <Counter/>
+    </>
+  )
+}
+
+export default App
---
+++ RT-practice/src/components/counter.jsx
@@ -0,0 +1,13 @@
+const counter = () => {
+  return (
+    <>
+    <h1>readux toolkit App</h1>
+     <h2>0</h2>
+    <button>increment</button>
+    <button>Decrement</button>
+    <button>reset</button>
+    </>
+  )
+}
+
+export default counter
